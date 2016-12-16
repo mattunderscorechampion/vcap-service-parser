bdc755c5c5ae326032f5c42b7b0c32c3be42a4eb
Matt Champion <mattunderscorechampion@gmail.com>
2016-12-16 22:30:59 +0000

    Enable linting.

--- node/gulpfile.js
+++ node/gulpfile.js
@@ -4,9 +4,27 @@
 var gulp = require('gulp'),
     jasmine = require('gulp-jasmine'),
     TerminalReporter = require('jasmine-terminal-reporter'),
-    reporters = require('jasmine-reporters');
+    reporters = require('jasmine-reporters'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
 
-gulp.task('unit-test', function(done) {
+var lintOptions = {
+    curly : true,
+    undef : true,
+    unused : false,
+    strict : true,
+    eqeqeq : true,
+    maxlen : 120,
+    maxdepth : 5,
+    indent : 4,
+    noempty : true,
+    nonew : true,
+    maxcomplexity : 12,
+    node : true,
+    esversion: 5
+};
+
+gulp.task('unit-test', function() {
     var reporter = new reporters.JUnitXmlReporter({
         savePath : "target/jasmine",
         filePrefix : "JUnit-",
@@ -21,4 +39,11 @@ gulp.task('unit-test', function(done) {
         }));
 });
 
-gulp.task('default', ['unit-test']);
+gulp.task('lint', function () {
+    return gulp.src(['src/main/*.js'])
+        .pipe(jshint(lintOptions))
+        .pipe(jshint.reporter(stylish))
+        .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('default', ['unit-test', 'lint']);
