1ee6f31909b87737062ba729dbeae7534cdafce2
Matt Champion <mattunderscorechampion@gmail.com>
2016-12-17 11:40:31 +0000

    Test string parsing with a more interesting value.

--- node/src/test/vcap-parser.spec.js
+++ node/src/test/vcap-parser.spec.js
@@ -17,9 +17,10 @@ describe('VCAP service parser', function() {
 
     it('can handle a string with the Reappt service', function() {
         var parser = new vcap.Parser([new reappt.Parser()]);
-        var services = parser.parse('{"push-reappt" : [{}]}');
+        var services = parser.parse('{"push-reappt":[{"credentials":{"host":"host","principal":"principal","credentials":"credentials"}}]}');
 
-        expect(services.reappt).toBe(null);
+        expect(services.reappt.host).toBe("host");
+        expect(services.reappt.principal).toBe("principal");
+        expect(services.reappt.credentials).toBe("credentials");
     });
-
 });
